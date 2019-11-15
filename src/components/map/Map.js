bf4e399210d5e00788964baa636ecc91854ab18f
Ashley Owens <ash4741@gmail.com>
2019-11-14 23:33:30 -0500

    Setup Basic App Structure

---
+++ src/components/map/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Map extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+        <h1>Hi</h1>
+      </>
+     );
+  }
+}
+ 
+export default Map;
